cb9d3e0ffc99690313fc56743f27106db88a179e
Cameron-Ross <cameron.edward.ross@gmail.com>
2022-11-06 20:36:52 -0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './App.css';
 import Piano from './Components/Piano';
 
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
--- src/Components/Key.tsx
+++ src/Components/Key.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import "../App.css";
 
+import * as Tone from "tone";
 
 function Key(props: {name: string}) {
 
     let isFlat: boolean = props.name.length > 1;
+    const synth = new Tone.Synth().toDestination();
+
+    function playSynth() {
+        console.log("clicked");
+        synth.triggerAttackRelease(props.name[0] + "4", "8n");
+    }
 
     return(
-        <div className= {isFlat ? "key flat" : "key"} onClick = {() => console.log(props.name)}>
+        <div className= {isFlat ? "key flat" : "key"} onClick = {() => playSynth()}>
              <div className="key-text">
                 {!isFlat ? props.name : ""}
             </div>
